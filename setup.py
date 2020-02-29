df03ced5cb7e39d3ec034d917b2cb575e5f0e978
surikoya@yahoo.com <surikoya@yahoo.com>
2020-02-28 16:21:09 -0800

    Basic python package

--- setup.py
+++ setup.py
@@ -9,35 +9,19 @@
 from setuptools import setup, find_packages
 
 
-requirements = [
-	'scipy', 'numpy', 'scikit-learn', 'Click'
-]
-
-test_requirements=[
-	'behave'
-]
-
 setup(
-	name='irisvmpy',
+	name='basicvmpy',
 	version='0.0.1',
-	description='SVM classifier for iris data-set',
-	author='Michal Dyzma',
-	author_email='mdyzma@gmail.com',
-    url ='https://github.com/mdyzma/jenkins-python-test',
-    download_url='https://github.com/mdyzma/jenkins-python-test/archive/0.0.1.tar.gz',
+	description='Basic python build',
+	author='Suresh Koya',
+	author_email="sk@gm.com',
 	license='MIT',
 	packages=find_packages(),
 	install_requires=requirements,
 	entry_points={
     	'console_scripts': [
-        		'irisvmpy = irisvmpy.iris:cli',
+        		'hello = basicvmpy.iris:cli',
         	],
         },
-	classifiers=[
-        'Development Status :: 1 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-      ],
 	zip_safe=False
 )
--- tests/features/iris.feature
+++
@@ -1,6 +0,0 @@
-Feature: irissvmpy performance
-
-  Scenario: run main program loop
-     Given we have irisvmpy installed
-      when we run program
-      then Something will be displayed
